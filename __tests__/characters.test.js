06bf3e11fc8dc0b1dd01cf9fc73b1fa298590f7b
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-08 12:04:11 -0800

    Add complete test for warrior hit function

--- __tests__/characters.test.js
+++ __tests__/characters.test.js
@@ -7,4 +7,22 @@ describe("Warrior", () => {
         expect(warrior.strength).toEqual(50);
         expect(warrior.intelligence).toEqual(10);
     });
+
+    test("should create a warrior hit function that effects recipients health based on attack", () => {
+        const warrior = new Warrior();
+        const mage = new Mage();
+        warrior.hit(mage);
+        expect(mage.health).toEqual(120);
+    });
+});
+
+import {Mage} from "../src/characters.js"
+describe("Sage", () => {
+    test("should create a sage object with health, attack, strength & intelligence", () => {
+        const mage = new Mage();
+        expect(mage.health).toEqual(150);
+        expect(mage.attack).toEqual(50);
+        expect(mage.strength).toEqual(35);
+        expect(mage.intelligence).toEqual(20);
+    });
 });
--- src/characters.js
+++ src/characters.js
@@ -5,4 +5,17 @@ export class Warrior {
         this.strength = 50;
         this.intelligence = 10;
     }
+
+    hit(recipient) {
+        recipient.health = recipient.health - this.attack;
+    }
+}
+
+export class Mage {
+    constructor(){
+        this.health = 150;
+        this.attack = 50;
+        this.strength = 35;
+        this.intelligence = 20;
+    }
 }
