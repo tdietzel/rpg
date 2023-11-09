b5e45593ecfe073d8e36b12c74dca3ece3967d0d
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-09 09:24:06 -0800

    Add killerBee class & constructor with health, attack & lvl

---
+++ __tests__/killerBee.test.js
@@ -0,0 +1,10 @@
+import {killerBee} from "../src/killerBee";
+
+describe("KillerBee", () => {
+    // constructor
+    test("should create a KillerBee object with health, attack & level", () =>{
+        expect(killerBee.health).toEqual(50);
+        expect(killerBee.attack).toEqual(10);
+        expect(killerBee.level).toEqual(1);
+    });
+});
---
+++ src/killerBee.js
@@ -0,0 +1,8 @@
+class KillerBee {
+    constructor(){
+        this.health = 50;
+        this.attack = 10;
+        this.level = 1;
+    }
+}
+export let killerBee = new KillerBee();
